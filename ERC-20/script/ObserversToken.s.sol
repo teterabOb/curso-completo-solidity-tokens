b1a3bd9abe673f9de7b2d7a7a495aa0e07a0ed41
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-05-16 22:28:40 -0400

    foundry installed and token added

--- ERC-20/script/ObserversToken.s.sol
+++ ERC-20/script/ObserversToken.s.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {Script, console2, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {ObserversToken} from "../src/ObserversToken.sol";
 
-contract CounterScript is Script {
+contract ObserversTokenScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         vm.startBroadcast(deployerPrivateKey);
-        Counter counter = new Counter();        
-        console.logAddress(address(counter));
+        ObserversToken token = new ObserversToken(address(this));
+        console.logAddress(token.owner());
         vm.stopBroadcast();
     }
 }
